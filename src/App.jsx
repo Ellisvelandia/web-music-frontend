0642e3c0238512490adb429f3aa81a07882992d6
Ellisvelandia <eyis619l@gmail.com>
2023-03-28 18:27:56 -0500

    website music

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Artist,
+  Browse,
+  Browser,
+  Charts,
+  Header,
+  Home,
+  Layout,
+  LayoutSidebar,
+} from "./router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="/browser"
+            element={
+              <Layout>
+                <Browser />
+              </Layout>
+            }
+          />
+          <Route
+            path="/charts"
+            element={
+              <Layout>
+                <Charts />
+              </Layout>
+            }
+          />
+          <Route
+            path="/artists"
+            element={
+              <Layout>
+                <Artist />
+              </Layout>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
